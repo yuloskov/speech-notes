0d2d2aeb7612a1f72866f729966055b231d4a098
Dantara <supercreed2@gmail.com>
2020-11-26 17:34:08 +0300

    Upload files

--- speech2text/app.py
+++ speech2text/app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+import os
+import secrets
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-STORAGE_DIR = "storage"
+STORAGE_FOLDER = "storage"
+app.config['STORAGE_FOLDER'] = STORAGE_FOLDER
 
-@app.route('/')
+ALLOWED_EXTENSIONS = {'flac', 'wav'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def update_filename(filename):
+    name, *extensions = filename.split('.')
+    new_name = secrets.token_hex(64)
+    return ".".join([new_name] + extensions)
+
+
+@app.route('/', methods=['POST'])
 def receive_file():
-    return 'Ok'
+    if 'audio_file' not in request.files:
+        return jsonify({"status": "error", "error_msg": "No file part"})
+
+    file = request.files['audio_file']
+
+    if file.filename == '':
+        return jsonify({"status": "error", "error_msg": "No file was sent"})
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        upd_filename = update_filename(filename)
+        file.save(os.path.join(app.config['STORAGE_FOLDER'], upd_filename))
+        return jsonify({"status": "ok"})
+
+    return jsonify({"status": "error", "error_msg": "Unsupported file extension"})
+
 
 @app.route('/status')
 def status():
-    return 'Ok'
+    return jsonify({"status": "ok"})
