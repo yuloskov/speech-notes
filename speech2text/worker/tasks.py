a66bdfc8659674f3ff234fa367b2759655976182
Dantara <supercreed2@gmail.com>
2020-12-01 21:23:03 +0300

    Recognition

--- speech2text/worker/tasks.py
+++ speech2text/worker/tasks.py
@@ -1,6 +1,7 @@
 import urllib.request
-
 from celery import Celery
+import speech_recognition as sr
+
 
 app = Celery('tasks', broker='amqp://localhost')
 app.autodiscover_tasks()
@@ -14,4 +15,12 @@ def proceed_audio(filename, callback_url):
     url = f'{PROTOCOL}://{SERVER_ADDRESS}:{SERVER_PORT}/{filename}'
     urllib.request.urlretrieve(url, filename)
 
-    return None
+    r = sr.Recognizer()
+    audio_file = sr.AudioFile(filename)
+    with audio_file as source:
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r.record(source)
+
+    ans = r.recognize_sphinx(audio)
+
+    return ans
